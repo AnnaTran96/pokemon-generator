243938be9e8b47137dd1b7b240b70e788f021bdf
Anna Tran <annatran@annas-air.mynet>
2020-07-25 18:43:07 +0100

    add event listener on select button

--- client/client.js
+++ client/client.js
@@ -1,4 +1,7 @@
 const startBtn = document.querySelector('.start')
+const selectBtn = document.querySelector('.select')
+const aBtn = document.querySelector('.a-btn')
+const BBtn = document.querySelector('.b-btn')
 
 // **********************  START BUTTON **********************
 
@@ -25,12 +28,41 @@ function hideText() {
   document.querySelector('.press-start').style.visibility = "hidden";
 }
 
+// function changeSize() {
+//   let image = document.querySelector('#photo')
+//     image.style.width = "50%";
+//     image.style.height = "50%"
+// }
+
 startBtn.addEventListener('click', function() {
   setInterval(function () {
       hideText();
       changeImage();
+      // changeSize();
   }, 100);
 })
 
+// **********************  SELECT BUTTON **********************
 
-// **********************  A BUTTON **********************
+selectBtn.addEventListener('click', backgroundBlur)
+
+function backgroundBlur() {
+  document.querySelector('#search').style.visibility = "visible";
+  document.querySelector('.background').style.filter = "blur(5px)";
+  document.querySelector('figurecaption').style.filter = "blur(5px)";
+  document.querySelector('h1').style.filter = "blur(5px)";
+  document.querySelector('.gameboy-body').style.filter = "blur(5px)";
+  document.querySelector('.black-screen').style.filter = "blur(5px)";
+}
+
+let random = Math.floor(Math.random() * imageArr.length)
+
+function choosePokemon(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then(console.log(`https://pokeapi.co/api/v2/pokemon/${id}`))
+      .then(response => response.json)
+      .then(pokemon => console.log(pokemon.name))
+      .catch( error => console.log("Error occurred."))
+}
+
+// create new event listener for submit button
