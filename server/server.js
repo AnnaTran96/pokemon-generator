0154b6052781bd0e8f634eab73bc742419bc9a52
Anna Tran <annatran@annas-air.mynet>
2020-07-25 13:42:48 +0100

    add listener

--- server/server.js
+++ server/server.js
@@ -3,13 +3,19 @@ const app = express();
 const port = 3000;
 const cors = require('cors')
 const bodyParser = require('body-parser')
+module.exports = app.listen()
 
 app.use(cors());
 app.use(bodyParser.text());
 
 app.get('/', (req, res) => {
-    console.log(res.statusCode)
-    res.send("Hello Pokemon Fan!")
+    res.send("Hello World")
 });
 
 app.listen(port,() => console.log(`App listening on: http://localhost:${port}`));
+
+
+
+//   //Launch listening server on port 8080
+//   app.listen(8080, function () {
+//     console.log('App listening on 
