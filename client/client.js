e78f8d2eb6878542ad84e079ff6785561a340a6a
Anna Tran <annatran@annas-air.mynet>
2020-07-25 21:17:46 +0100

    create pause function

--- client/client.js
+++ client/client.js
@@ -3,11 +3,14 @@ const selectBtn = document.querySelector('.select')
 const aBtn = document.querySelector('.a-btn')
 const bBtn = document.querySelector('.b-btn')
 const backBtn = document.querySelector('.back-btn')
+const backBtn2 = document.querySelector('.back-btn2')
+
+
 
 // **********************  START BUTTON **********************
 
 let imageArr = new Array();
-for(var i = 1; i < 15; i++) {
+for(var i = 1; i < 53; i++) {
   imageArr.push(`assets/img${i}.png`)
 }
 
@@ -25,23 +28,38 @@ function changeImage() {
   }
 }
 
-function hideText() {
+function hideTextShowPhoto() {
   document.querySelector('.press-start').style.visibility = "hidden";
+   document.querySelector('#pokeball').style.visibility = "hidden";
+  //  document.querySelector('#photo').style.visibility = "visible";
+
 }
 
-// function changeSize() {
-//   let image = document.querySelector('#photo')
-//     image.style.width = "50%";
-//     image.style.height = "50%"
-// }
+let timer = null;
+
+function startInterval() {
+  timer = setInterval(function () {
+    hideTextShowPhoto();
+    changeImage();
+}, 20);
+}
+
+function stopInterval() {
+  clearInterval(timer)
+}
 
 startBtn.addEventListener('click', function() {
-  setInterval(function () {
-      hideText();
-      changeImage();
-      // changeSize();
-  }, 100);
-})
+  document.querySelector('#photo').style.visibility = "visible";
+  startInterval()
+}) // add {once:true}
+
+
+
+// **********************  A BUTTON **********************
+
+aBtn.addEventListener('click', stopInterval)
+
+
 
 // **********************  SELECT BUTTON **********************
 
@@ -49,6 +67,7 @@ selectBtn.addEventListener('click', backgroundBlur)
 
 function backgroundBlur() {
   document.querySelector('#search').style.visibility = "visible";
+  // document.querySelector(".images").removeEventListener("mousemove", changeImage);
   document.querySelector('.background').style.filter = "blur(5px)";
   document.querySelector('figurecaption').style.filter = "blur(5px)";
   document.querySelector('h1').style.filter = "blur(5px)";
@@ -78,3 +97,30 @@ function choosePokemon(id) {
 }
 
 // create new event listener for submit button
+// createe function to stop start button
+
+
+
+// **********************  B BUTTON **********************
+
+bBtn.addEventListener('click', () => {
+  document.querySelector('.zubat').style.visibility = "visible";
+  document.querySelector('.background').style.filter = "blur(5px)";
+  document.querySelector('figurecaption').style.filter = "blur(5px)";
+  document.querySelector('h1').style.filter = "blur(5px)";
+  document.querySelector('.gameboy-body').style.filter = "blur(5px)";
+  document.querySelector('.black-screen').style.filter = "blur(5px)";
+}) 
+
+backBtn2.addEventListener('click', goBack2)
+
+function goBack2() {
+  document.querySelector('.zubat').style.visibility = "hidden";
+  document.querySelector('.background').style.filter = "blur(0px)";
+  document.querySelector('figurecaption').style.filter = "blur(0px)";
+  document.querySelector('h1').style.filter = "blur(0px)";
+  document.querySelector('.gameboy-body').style.filter = "blur(0px)";
+  document.querySelector('.black-screen').style.filter = "blur(0px)";
+}
+
+
